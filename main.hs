8b1c19417a66e7d6d77aed1a40ab683fee5bd50a
Vincent Woltmann <vincent.woltmann@hu-berlin.de>
2019-07-08 23:37:14 +0200

    Changed Structure and added filters

--- main.hs
+++ main.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+
 import Data.Ix
 import Data.Aeson
+import Data.List
+import Data.Maybe
 import GHC.Generics
 import qualified Data.ByteString.Lazy as Lazy
 import Data.Char (isDigit)
 import System.IO
 import Control.Monad
 import Data.Char
+import System.Environment
 
 data Year = Year Int
      deriving (Eq, Ord, Generic)
@@ -49,7 +53,7 @@ instance Show Author where
     show (Author name birth Nothing)
         = name ++ "(*" ++ show birth ++ ")"
     show (Author name birth (Just death))
-        = name ++ "(*" ++ show birth ++ ",†" ++ show death ++ ")"
+        = name ++ "(*" ++ show birth ++ ", †" ++ show death ++ ")"
 
 data Book = Book {title :: String,
                   author :: Author,
@@ -62,8 +66,8 @@ instance ToJSON Book
 instance Show Book where
     show (Book title author published)
         = title ++ " von " ++ (show author) ++ ", erschienen " ++ show published
-
-
+instance Ord Book where
+    (Book titleA _ _) `compare` (Book titleB _ _) = titleA `compare` titleB
 
 günther_grass = Author "Günther Grass" (Year 1927) $ Just $ Year 2015
 blech_trommel = Book "Die Blechtrommel" günther_grass $ Year 1959
@@ -77,69 +81,64 @@ readBooksFile = Lazy.readFile booksFile
 readBooks :: IO (Either String [Book])
 readBooks = (eitherDecode <$> readBooksFile)
 
-printBooks :: IO ()
-printBooks = do
+printBooks :: [String] -> IO ()
+printBooks arguments = do
+    
+    let author_filter
+            = map toLower $ fromMaybe "" $ listToMaybe author_predicates
+                where author_predicates
+                        = filter ("--author=" `isPrefixOf`) arguments
+    let title_filter
+            = map toLower $ fromMaybe "" $ listToMaybe title_predicates
+                where title_predicates
+                        = filter ("--title=" `isPrefixOf`) arguments
     books <- readBooks
+
     case books of 
-        Left error -> putStrLn error
-        Right books -> mapM_ print books
+        Left error
+            -> putStrLn error
+        Right books
+            -> mapM_ print $ filter predicate books
+                where predicate book
+                        = (drop 9 author_filter) `isSubsequenceOf` author_name
+                          &&
+                          (drop 8 title_filter) `isSubsequenceOf` book_title
+                            where author_name = map toLower $ name $ author book
+                                  book_title = map toLower $ title book
     return ()
 
 writeBooks :: [Book] -> IO ()
 writeBooks books = 
-    Lazy.writeFile booksFile (encode books)
+    Lazy.writeFile booksFile $ encode $ sort books
 
-readDeath :: IO (Maybe Year)
-readDeath = do
-    putStrLn "Lebt der Autor noch? (J/N)"
-    answer <- getLine
-    case answer of
-        "J" -> return Nothing
-        "N" -> do
-            putStrLn "In welchem Jahr ist er gestorben?"
-            death <- readLn :: IO Year
-            return $ Just death
-        _ -> readDeath
+addBook :: [String] -> IO ()
+addBook (title:published:name:birth:death:_) = do
 
-addBook :: IO ()
-addBook = do
-    putStrLn "Wie lautet der Titel des Buches?"
-    title <- getLine
-    putStrLn "In welchem Jahr wurde das Buch veröffentlicht?"
-    published <- readLn :: IO Year
-    putStrLn "Wie lautet der Name des Autors?"
-    name <- getLine
-    putStrLn "In welchem Jahr wurde der Autor geboren?"
-    birth <- readLn :: IO Year
-    death <- readDeath
-
-    let author = Author name birth death
-    let book = Book title author published
+    let author = Author name (read birth :: Year) (Just $ read death :: Maybe Year)
+    let book = Book title author $ read published
 
     current_books <- readBooks
     case current_books of
-        Left error -> putStrLn error
-        Right books -> writeBooks (book : books)
-    putStrLn "Buch wurde erfolgreich hinzugefügt"
+        Left error  -> do putStrLn error
+        Right books -> do writeBooks (book : books)
+                          putStrLn "Buch wurde erfolgreich hinzugefügt"
+addBook _ = do
+    putStrLn "Nicht genügend Argumente"
+
+unknownAction :: String -> IO ()
+unknownAction action =
+    putStrLn $ "Unbekannte Aktion: " ++ action
+
+dispatch :: String -> [String] -> IO ()
+dispatch "view" = printBooks
+dispatch "add"  = addBook
+dispatch action = const (unknownAction action)
 
 main :: IO ()
 main = do
-    putStrLn "Willkommen im Buch-Manager"
-    putStrLn "Was willst du tun?"
-    putStrLn "(1) Alle Bücher ausgeben"
-    putStrLn "(2) Ein Buch hinzufügen"
-    putStrLn "(3) Beenden"
-    putStr "( )\b\b"
-    hFlush stdout
-    choice <- readLn :: IO Int
-    case choice of
-        1 -> do
-            putStrLn ""
-            printBooks
-            putStrLn ""
-            main
-        2 -> do addBook; main
-        3 -> return ()
-        _ -> main
-
-
+    arguments <- getArgs
+    case arguments of
+        (action:parameters)
+            -> dispatch action parameters
+        []                 
+            -> putStrLn "Bitte gib eine Aktion an."
